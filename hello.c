a6de3ab55a2c0ac6140da9b0b2ce72f62908b06d
chixian6940 <165387814+chixian6940@users.noreply.github.com>
2024-08-06 00:57:52 +0800

    Update hello.c
    更新计算式

--- hello.c
+++ hello.c
@@ -1,6 +1,17 @@
 #include<stdio.h>
-
+int max(int a,int b)
+{
+    if(a>b)
+        return a;
+    else
+        return b;
+}
 int main(void)
 {
-    printf("Hello,World!\n");
+    //printf("Hello,World!\n");
+    int a=20;
+    int b=30;
+    int c;
+    c=max(a,b);
+    printf("%d is the max num!",c);
 }
